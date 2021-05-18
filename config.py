5019210cda6a5319f5b87a852c4a978097b005dd
wituwitu <wistuba@niclabs.cl>
2021-05-17 20:49:33 -0400

    rerunning 150b 20p

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 exp_pairs = [#('Packets/77_bytes.txt', 20),
-             ('Packets/150_bytes.txt', 10),
-             ('Packets/231_bytes.txt', 10),
-             ('Packets/512_bytes.txt', 10)]
+             ('Packets/150_bytes.txt', 10)#,
+             #('Packets/231_bytes.txt', 10),
+             #('Packets/512_bytes.txt', 10)
+             ]
